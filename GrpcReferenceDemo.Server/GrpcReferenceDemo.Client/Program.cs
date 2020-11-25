a1b3d0c7fb1657eef22dd8f5f0c7b55803375445
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-11-25 18:36:31 +0100

    gRPC added

---
+++ GrpcReferenceDemo.Server/GrpcReferenceDemo.Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcReferenceDemo.Shared;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcReferenceDemo.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Prepare connection
+            GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+
+            // Prepare stream client
+            TestStreamer.TestStreamerClient streamClient = 
+                new TestStreamer.TestStreamerClient(channel);
+
+            // Get reply stream
+            AsyncServerStreamingCall<ThingReply> stream = 
+                streamClient.DoAThing(new ThingRequest { Amount = 100 });
+
+            // Print data while stream is alive.
+            await foreach (var response in stream.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"Latest: {response.Latest} - Sqrt: {response.Sqrt}");
+            }
+
+            // Prevent close
+            Console.WriteLine("Server is done sending. Goodbye.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcReferenceDemo.Server/GrpcReferenceDemo.Server/Services/TestStreamerService.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using GrpcReferenceDemo.Shared;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcReferenceDemo.Server
+{
+    public class TestStreamerService : TestStreamer.TestStreamerBase
+    {
+        private readonly ILogger<TestStreamerService> _logger;
+
+        public TestStreamerService(ILogger<TestStreamerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task DoAThing(ThingRequest request, IServerStreamWriter<ThingReply> responseStream, ServerCallContext context)
+        {
+            for (int current = 0; current < 100; current++)
+            {
+                // Do stuff and prepare the/a reponse message
+                var reply = new ThingReply {
+                    Latest = current,
+                    Sqrt = Math.Sqrt(current)
+                };
+
+                // Send response message & wait
+                await responseStream.WriteAsync(reply);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
